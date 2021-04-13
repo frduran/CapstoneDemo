3c3711fca058671c666657711d1ae944e203b6ec
frduran <frduran@uc.cl>
2021-04-12 21:44:53 -0400

    added port to config

--- config/config.js
+++ config/config.js
@@ -1,25 +1,28 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-  "development": {
-    "username": process.env.USERNAME,
-    "password": process.env.PASSWORD,
-    "database": process.env.DB,
-    "host": process.env.HOST,
-    "dialect": process.env.DIALECT
+  development: {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DB,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+    port: process.env.PORT,
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DB,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+    port: process.env.PORT,
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+  production: {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DB,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+    port: process.env.PORT,
+  },
+};
